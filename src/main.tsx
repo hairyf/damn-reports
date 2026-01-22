1803f71c44cf638f259a7e9850a9295bb0ee7aba
hairyf <wwu710632@gmail.com>
2026-01-22 09:50:09 +0800

    refactor: remove unused Tauri configuration and implement tray functionality
    - Deleted the obsolete `tary.ts` configuration file, which was previously handling tray icon setup.
    - Integrated tray icon functionality directly into the `lib.rs` file, enhancing the application's structure.
    - Added utility functions for window navigation and display management, improving code organization and reusability.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { Provider } from './provider.tsx'
-import './config/tary.ts'
 import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -5,14 +5,91 @@ mod database;
 mod n8n;
 mod schedule;
 mod task;
+mod utils;
 
-use tauri::{ipc::Invoke, Wry};
+use tauri::{
+    ipc::Invoke,
+    menu::{Menu, MenuEvent, MenuItem},
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    Manager, Runtime, Wry,
+};
 use tauri_plugin_sql::{Migration, MigrationKind};
+use utils::{navigate, show_window};
 
 // setup app
 fn setup(app_handle: tauri::AppHandle) {
     let _ = command::database_loaded(app_handle);
     let _ = n8n::start();
+}
+
+fn tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
+    // 使用默认窗口图标
+    let icon = app.default_window_icon().unwrap().clone();
+
+    // 构建菜单
+    let menu = Menu::with_items(
+        app,
+        &[
+            &MenuItem::with_id(app, "overview", "打开面板", true, None::<&str>)?,
+            &MenuItem::with_id(app, "reports", "报告列表", true, None::<&str>)?,
+            &MenuItem::with_id(app, "settings", "设置", true, None::<&str>)?,
+            &MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?,
+        ],
+    )?;
+
+    fn handle_menu_event<R: Runtime>(app: &tauri::AppHandle<R>, event: &MenuEvent) {
+        match event.id().as_ref() {
+            "overview" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    navigate(&window, "/");
+                    show_window(&window);
+                }
+            }
+            "reports" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    navigate(&window, "/report");
+                    show_window(&window);
+                }
+            }
+            "settings" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    navigate(&window, "/setting");
+                    show_window(&window);
+                }
+            }
+            "quit" => {
+                app.exit(0);
+            }
+            _ => {}
+        }
+    }
+
+    fn handle_tray_icon_event<R: Runtime>(tray: &tauri::tray::TrayIcon<R>, event: &TrayIconEvent) {
+        let app = tray.app_handle();
+        match event {
+            TrayIconEvent::Click {
+                button: MouseButton::Left,
+                ..
+            } => {
+                if let Some(window) = app.get_webview_window("main") {
+                    show_window(&window);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    // 构建托盘图标
+    let _ = TrayIconBuilder::new()
+        .icon(icon)
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .tooltip("Damn Daily Reports")
+        .on_menu_event(move |app, event| handle_menu_event(app, &event))
+        .on_tray_icon_event(move |tray, event| handle_tray_icon_event(&tray, &event))
+        .build(app)?;
+
+    Ok(())
 }
 
 // configure handler
@@ -30,41 +107,40 @@ fn handler() -> impl Fn(Invoke<Wry>) -> bool + Send + Sync + 'static {
 
 // configure builder
 fn builder() -> tauri::Builder<tauri::Wry> {
-    let migrations = vec![
-        Migration {
-            version: 1,
-            description: "initialize database",
-            sql: include_str!("../prisma/migrations/20260118100407/migration.sql"),
-            kind: MigrationKind::Up,
-        }
-    ];
+    let migrations = vec![Migration {
+        version: 1,
+        description: "initialize database",
+        sql: include_str!("../prisma/migrations/20260118100407/migration.sql"),
+        kind: MigrationKind::Up,
+    }];
     tauri::Builder::default()
-    // Simple Store plugin
-    .plugin(tauri_plugin_store::Builder::new().build())
-    // Sql store plugin
-    .plugin(
-        tauri_plugin_sql::Builder::default()
-            .add_migrations("sqlite:main.db", migrations)
-            .build(),
-    )
-    // HTTP plugin
-    .plugin(tauri_plugin_http::init())
-    // Notification plugin
-    .plugin(tauri_plugin_notification::init())
-    // Opener plugin
-    .plugin(tauri_plugin_opener::init())
-    // Dialog plugin
-    .plugin(tauri_plugin_dialog::init())
-    // FS plugin
-    .plugin(tauri_plugin_fs::init())
-    // Process plugin
-    .plugin(tauri_plugin_process::init())
+        // Simple Store plugin
+        .plugin(tauri_plugin_store::Builder::new().build())
+        // Sql store plugin
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:main.db", migrations)
+                .build(),
+        )
+        // HTTP plugin
+        .plugin(tauri_plugin_http::init())
+        // Notification plugin
+        .plugin(tauri_plugin_notification::init())
+        // Opener plugin
+        .plugin(tauri_plugin_opener::init())
+        // Dialog plugin
+        .plugin(tauri_plugin_dialog::init())
+        // FS plugin
+        .plugin(tauri_plugin_fs::init())
+        // Process plugin
+        .plugin(tauri_plugin_process::init())
 }
 
 // run app
 pub fn run() {
     builder()
         .setup(|app| {
+            tray(&app.handle()).unwrap();
             setup(app.handle().clone());
             Ok(())
         })
