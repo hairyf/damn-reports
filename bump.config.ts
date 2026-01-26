3a1017487da7dc92543fe3b2b35e1916901497fb
hairyf <wwu710632@gmail.com>
2026-01-26 10:08:51 +0800

    chore: simplify bump configuration by removing unnecessary options
    - Removed the `commit`, `tag`, and `push` options from `bump.config.ts`, streamlining the release configuration for better clarity and usability.

--- bump.config.ts
+++ bump.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'bumpp'
 
 export default defineConfig({
   release: 'prompt',
-  commit: false,
-  tag: false,
-  push: false,
   files: [
     'package.json',
     'tauri/Cargo.toml',
