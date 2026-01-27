cfb67d99ea596cfe6bdb410a204d6bf152055618
hairyf <wwu710632@gmail.com>
2026-01-27 17:28:17 +0800

    chore: update bump configuration and add patch for bumpp to support tauri configuration files

--- bump.config.ts
+++ bump.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'bumpp'
 
 export default defineConfig({
   release: 'prompt',
+  commit: true,
+  push: false,
+  tag: false,
   files: [
     'package.json',
     'tauri/Cargo.toml',
+    'tauri/Cargo.lock',
     'tauri/tauri.conf.json',
   ],
 })
