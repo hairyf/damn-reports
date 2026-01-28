28f886d9ff6142d4cba3f705c3646b55df064472
hairyf <wwu710632@gmail.com>
2026-01-28 21:47:23 +0800

    fix: comment out unused openUrl function and add content security policy for n8n service

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
-import { openUrl } from '@tauri-apps/plugin-opener'
+// import { openUrl } from '@tauri-apps/plugin-opener'
 
 export type SiteConfig = typeof siteConfig
 export const siteConfig = {
@@ -24,14 +24,14 @@ export const siteConfig = {
     {
       label: '工作流',
       href: '/workflow',
-      onClick: () => {
-        const params = new URLSearchParams([
-          ['email', store.n8n.email || N8N_LOGIN_DATA.emailOrLdapLoginId],
-          ['password', store.n8n.password || N8N_LOGIN_DATA.password],
-          ['hideUI', 'true'], // 通过 URL 参数告诉 n8n 隐藏 UI 元素
-        ])
-        openUrl(`http://localhost:5678/workflow/${store.n8n.workflow}?${params.toString()}`)
-      },
+      // onClick: () => {
+      //   const params = new URLSearchParams([
+      //     ['email', store.n8n.email || N8N_LOGIN_DATA.emailOrLdapLoginId],
+      //     ['password', store.n8n.password || N8N_LOGIN_DATA.password],
+      //     ['hideUI', 'true'], // 通过 URL 参数告诉 n8n 隐藏 UI 元素
+      //   ])
+      //   openUrl(`http://localhost:5678/workflow/${store.n8n.workflow}?${params.toString()}`)
+      // },
     },
     {
       label: '设置',
