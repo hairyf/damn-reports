7fbc3a2838e3b7a650c3f6491d8894f6eb74ac19
hairyf <wwu710632@gmail.com>
2026-04-29 12:18:46 +0800

    feat(ui): change ui components path

--- src/app/(static)/components/sidebar/sidebar.tsx
+++ src/app/(static)/components/sidebar/sidebar.tsx
@@ -2,7 +2,6 @@
 
 import { ChartDonut, ChartPie, FileText, House, Magnifier, Plus, SparklesFill, SquareChartBar } from '@gravity-ui/icons'
 import { Button } from '@heroui/react'
-import { Menu } from '../../../components/menu'
 import { Topbar } from './topbar'
 
 export function Sidebar() {
@@ -11,10 +10,10 @@ export function Sidebar() {
       <Topbar />
       <Menu className="px-1">
         <Menu.Group>
-          <Menu.Item icon={<Magnifier className="size-3.5 text-foreground/70" />}>
+          <Menu.Item icon={<Magnifier className="size-3.5" />}>
             Search
           </Menu.Item>
-          <Menu.Item icon={<House className="size-3.5 text-foreground/70" />}>
+          <Menu.Item icon={<House className="size-3.5" />}>
             Home
           </Menu.Item>
         </Menu.Group>
@@ -27,13 +26,13 @@ export function Sidebar() {
           )}
         >
           <Menu.Collapse label="日报系统" icon={<SquareChartBar />}>
-            <Menu.Item icon={<ChartPie className="size-3.5 text-foreground/70" />}>
+            <Menu.Item icon={<ChartPie className="size-3.5" />}>
               概览
             </Menu.Item>
-            <Menu.Item icon={<FileText className="size-3.5 text-foreground/70" />}>
+            <Menu.Item icon={<FileText className="size-3.5" />}>
               报告
             </Menu.Item>
-            <Menu.Item icon={<ChartDonut className="size-3.5 text-foreground/70" />}>
+            <Menu.Item icon={<ChartDonut className="size-3.5" />}>
               来源
             </Menu.Item>
           </Menu.Collapse>
@@ -46,10 +45,10 @@ export function Sidebar() {
             </Button>
           )}
         >
-          <Menu.Item icon={<SparklesFill className="size-3.5 text-foreground/70" />}>
+          <Menu.Item icon={<SparklesFill className="size-3.5" />}>
             Diana AI
           </Menu.Item>
-          <Menu.Item icon={<SparklesFill className="size-3.5 text-foreground/70" />}>
+          <Menu.Item icon={<SparklesFill className="size-3.5" />}>
             Computer AI
           </Menu.Item>
         </Menu.Group>
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,101 +1,123 @@
-'use client'
+import type { PropsWithDetailedHTML } from '@hairy/react-lib'
+import type { PropsWithChildren } from 'react'
+import { ChevronRight } from '@gravity-ui/icons'
+import { Button, Chip, cn } from '@heroui/react'
+import clsx from 'clsx'
+import { CollapseTransition } from './collapse-transition'
 
-import type { ButtonProps } from '@heroui/react'
-import type { PropsWithChildren, ReactNode } from 'react'
-import { TriangleRightFill } from '@gravity-ui/icons'
-import { Button } from '@heroui/react'
-import { clsx } from 'clsx'
-import { AnimatePresence, motion } from 'motion/react'
-
-export function Menu(props: PropsWithChildren) {
+export function Menu(props: PropsWithChildren<PropsWithDetailedHTML>) {
   return (
-    <menu className="flex flex-col gap-2 px-1.5">
-      {props.children}
-    </menu>
-  )
-}
-
-Menu.Item = MenuItem
-Menu.Group = MenuGroup
-Menu.Expand = MenuExpand
-
-export interface MenuItemProps extends Omit<ButtonProps, 'children'> {
-  prefix?: ReactNode
-  suffix?: ReactNode
-  children?: ReactNode
-  to?: string
-}
-
-function MenuItem(props: MenuItemProps) {
-  return (
-    <Button className="w-full justify-between rounded-md" variant="ghost">
-      <div className="flex gap-2">
-        {props.prefix}
-        {props.children}
-      </div>
-      {props.suffix}
-    </Button>
-  )
-}
-
-export interface MenuGroupProps {
-  suffix?: ReactNode
-  label?: string
-  children?: ReactNode
-  defaultOpen?: boolean
-}
-
-function MenuGroup(props: MenuGroupProps) {
-  return (
-    <div className="flex flex-col">
+    <div className={cn('flex flex-col gap-1.5', props.className)}>
       {props.children}
     </div>
   )
 }
 
-export interface MenuExpandProps {
-  suffix?: ReactNode
+Menu.Item = MenuItem
+Menu.Group = MenuGroup
+Menu.Collapse = MenuCollapse
+
+export interface MenuGroupProps extends PropsWithChildren {
   label?: string
-  children?: ReactNode
-  defaultOpen?: boolean
+  extra?: React.ReactNode
 }
 
-function MenuExpand(props: MenuExpandProps) {
-  const [expanded, setExpanded] = useState(props.defaultOpen)
+export function MenuGroup(props: MenuGroupProps) {
   return (
-    <div className="flex flex-col">
+    <div className={clsx('flex flex-col', props.label ? 'gap-1.5' : 'gap-0.5')}>
+      {props.label
+        ? (
+            <>
+              <div className="flex justify-between items-center mr-2">
+                <Chip className="bg-transparent rounded-none mt-1.5 text-xs text-foreground/70">
+                  {props.label}
+                </Chip>
+                {props.extra}
+              </div>
+
+              <div className="flex flex-col gap-0.5">
+                {props.children}
+              </div>
+            </>
+          )
+        : props.children}
+    </div>
+  )
+}
+
+export interface MenuItemProps extends PropsWithChildren {
+  icon?: React.ReactNode
+  label?: React.ReactNode
+  suffix?: React.ReactNode
+  extra?: React.ReactNode
+}
+
+export function MenuItem(props: MenuItemProps) {
+  return (
+    <div className="item flex">
       <Button
-        variant={expanded ? 'tertiary' : 'ghost'}
-        className="w-full justify-between rounded-md"
+        className="flex-1 justify-start font-medium gap-3"
+        variant="ghost"
         size="sm"
-        onClick={() => setExpanded(!expanded)}
       >
-        <div className="flex gap-2 items-center">
-          <span className="text-xs">{props.label}</span>
-        </div>
-        <div className="flex gap-2">
-          {props.suffix}
-          <TriangleRightFill
-            className={clsx('size-2 pt-px transition-transform duration-300', {
-              'rotate-90': expanded,
-            })}
-          />
-        </div>
-      </Button>
-      <AnimatePresence initial={false}>
-        {expanded && (
-          <motion.div
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: 'auto', opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-            transition={{ duration: 0.2 }}
-          >
-            <div className="flex flex-col gap-1 mt-1">
-              {props.children}
-            </div>
-          </motion.div>
+        {props.icon && (
+          <span className=" text-foreground/50">
+            {props.icon}
+          </span>
         )}
-      </AnimatePresence>
+
+        <span className="flex-1 text-start">
+          {props.label || props.children}
+        </span>
+
+        {props.suffix && (
+          <span className="text-xs text-foreground/50">{props.suffix}</span>
+        )}
+        {props.extra && (
+          <div className="hidden item-hover:flex items-center gap-1">
+            {props.extra}
+          </div>
+        )}
+      </Button>
+    </div>
+  )
+}
+
+export interface MenuCollapseProps extends PropsWithChildren {
+  icon?: React.ReactNode
+  label?: React.ReactNode
+}
+
+export function MenuCollapse(props: MenuCollapseProps) {
+  const [visible, setVisible] = useState(false)
+  return (
+    <div className="flex flex-col gap-0.5">
+      <Button
+        className="w-full justify-start font-medium gap-3"
+        size="sm"
+        variant={visible ? 'tertiary' : 'ghost'}
+        onClick={() => setVisible(!visible)}
+      >
+        {props.icon && (
+          <span className=" text-foreground/50">
+            {props.icon}
+          </span>
+        )}
+        <span className="flex-1 text-start">{props.label}</span>
+        <ChevronRight
+          className={clsx(
+            'size-3 text-foreground/50',
+            'transition-transform duration-100',
+            visible && 'rotate-90',
+          )}
+        />
+
+      </Button>
+      <CollapseTransition visible={visible}>
+        <div className="flex flex-col [&>.item]:-mt-px [&>.item]:border-l [&>.item]:ml-2 [&>.item]:pl-1">
+          {props.children}
+        </div>
+      </CollapseTransition>
     </div>
   )
 }
